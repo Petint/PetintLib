14d9368689632e3edebc99247d8b357055aac53c
Petint <petras.balint04@gmail.com>
2022-06-17 22:23:31 +0200

    asd

--- PetintLib/__init__.py
+++ PetintLib/__init__.py
@@ -1,5 +1,4 @@
 """
-PetintLib 1.3.10
 A saját python Csomagom hasznos modulokkal, metódusokkal, osztályokkal és konstansokkal
 """
 
@@ -7,4 +6,4 @@ from . import gagyimatek
 from .autotable import Table
 from . import petint
 
-__version__ = {'core': '2.0.1', 'autotable': autotable.__version__, 'petint': petint.__version__, 'gagyimatek': gagyimatek.__version__}
+__version__ = {'core': '1.4.0', 'autotable': autotable.__version__, 'petint': petint.__version__, 'gagyimatek': gagyimatek.__version__}
--- tests/TableToFile.py
+++ tests/TableToFile.py
@@ -2,6 +2,6 @@ from PetintLib.autotable import Table
 
 size = 5
 test_data = [[x * size + y for y in range(size)] for x in range(size)]
-table1 = Table(test_data, length=3, align='c')
+table1 = Table(test_data, width=3, align='c')
 with open("table.txt", "w", encoding="utf-8") as f:
     f.write(table1.make())
