8b1c1bb322d24d9ac05fca2934e21df7e479bff3
Petint <petras.balint04@gmail.com>
2022-09-15 18:21:18 +0200

    PetintLib should be installable.

--- PetintLib/__init__.py
+++ PetintLib/__init__.py
@@ -6,4 +6,22 @@ from . import gagyimatek
 from .autotable import Table
 from . import petint
 
-__version__ = {'core': '1.4.0', 'autotable': autotable.__version__, 'petint': petint.__version__, 'gagyimatek': gagyimatek.__version__}
+__project__      = 'PetintLib'
+__version__ = '1.4.2'
+__keywords__     = ['Petint', 'Petras', 'Lib', 'Balint']
+__author__       = 'Petras Balint'
+__author_email__ = 'petras.balint04@gmail.com'
+__url__          = 'https://github.com/Petint'
+__platforms__    = 'ALL'
+
+__classifiers__ = [
+    "Development Status :: 4 - Beta",
+    "Topic :: Libraries",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+]
+
+__entry_points__ = {}
+
+__requires__ = []
+__extra_requires__ = {}
--- tests/TableToFile.py
+++ tests/TableToFile.py
@@ -1,7 +1,13 @@
-from PetintLib.autotable import Table
+from PetintLib import Table
 
-size = 5
-test_data = [[x * size + y for y in range(size)] for x in range(size)]
-table1 = Table(test_data, width=3, align='c')
-with open("table.txt", "w", encoding="utf-8") as f:
-    f.write(table1.make())
+
+def main():
+    size = 5
+    test_data = [[x * size + y for y in range(size)] for x in range(size)]
+    table1 = Table(test_data, width=3, align='wt')
+    with open("table.txt", "w", encoding="utf-8") as f:
+        f.write(table1.make())
+
+
+if __name__ == '__main__':
+    main()
