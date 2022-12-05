a64d0e76536685a54f51e7a63f506f21996dc632
Petrás Bálint <petras.balint04@gmail.com>
2022-12-05 19:37:50 +0100

    Take a dump in json

--- PetintLib/washingmachine.py
+++ PetintLib/washingmachine.py
@@ -1,9 +1,19 @@
 __version__ = '0.0.5'
-
+import json
 
 def clean(file_in: str):
     with open(file_in, 'rt', encoding='utf-8') as du:
         data = du.read().splitlines()
     data = [n.split(',') for n in data]
-    with open(file_in+'_clean', 'wt', encoding='utf-8') as du:
-        du.write(str(data))
+    keys = [f[0]for f in data[:13]]
+    vaules = [f[1]for f in data[:12]]
+    waveform_data = [float(f[0]) for f in data[13:]]
+    vaules.append(waveform_data)
+    print(keys, vaules, sep='\n')
+    d = {}
+    for i in keys:
+        for j in vaules:
+            d[i] = j
+    y = json.dumps(d)
+    print(y)
+
