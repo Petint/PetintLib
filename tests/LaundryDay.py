94973344e7d5a21f7f830f250db1b1f697ff8f9e
Petrás Bálint <petras.balint04@gmail.com>
2022-12-06 14:40:39 +0100

    clean() outputs data directly now

--- tests/LaundryDay.py
+++ tests/LaundryDay.py
@@ -1,16 +1,10 @@
 from PetintLib import washingmachine as wm
 import json
 
+
 def main():
-    original = wm.clean('test.csv')
-    with open('test.json', 'rt', encoding='utf-8') as tf:
-        read = json.load(tf)
-    print(read)
-    if read == original:
-        result = 'passed'
-    else:
-        result = 'failed'
-    print(f"Test {result} successfully")
+    param, data = wm.clean('test.csv')
+    print(param, data, sep="\n")
 
 
 if __name__ == '__main__':
