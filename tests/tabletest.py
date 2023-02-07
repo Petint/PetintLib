a1c201922732f8d56e3b2752959954f3683040e8
Petrás Bálint <petras.balint04@gmail.com>
2023-02-07 15:37:31 +0100

    autotable profiler

--- tests/tabletest.py
+++ tests/tabletest.py
@@ -1,4 +1,6 @@
 import PetintLib
+import cProfile
+import pstats
 
 
 def main():
@@ -10,5 +12,17 @@ def main():
     print(table1.make())
 
 
+def profile():
+    data = [[0x0, 0x1, 0x2, 3], [0x4, 0x5, 0x6, 0x7], [0x8, 0x9, 0xA, 0xB], [0xC, 0xD, 0xE, 0xF]]
+    table = PetintLib.Table(data, height=1, width='auto', align='cc')
+    with cProfile.Profile() as pr:
+        string_table = table.make()
+        print(string_table)
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    stats.dump_stats(filename='autotable.prof')
+
+
 if __name__ == '__main__':
     main()
+    profile()
